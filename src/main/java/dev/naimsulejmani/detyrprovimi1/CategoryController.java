ad889a0521680724269b2d97b169b98e88b05abd
Naim Sulejmani <naim.sulejmani@gmail.com>
2025-11-04 19:18:42 +0100

    implement category management features including search, edit, show, and delete functionalities

--- src/main/java/dev/naimsulejmani/detyrprovimi1/CategoryController.java
+++ src/main/java/dev/naimsulejmani/detyrprovimi1/CategoryController.java
@@ -23,8 +23,15 @@ public class CategoryController {
     }
 
     @GetMapping("")
-    public String list(Model model) {
-        model.addAttribute("categories", categories);
+    public String list(@RequestParam(required = false) String searchText, Model model) {
+        var result = categories;
+        if (searchText != null && !searchText.isBlank()) {
+            result = categories.stream().filter(cat ->
+                    cat.getName().toLowerCase().contains(searchText.toLowerCase()) ||
+                            cat.getDescription().toLowerCase().contains(searchText.toLowerCase())).toList();
+        }
+        model.addAttribute("categories", result);
+        model.addAttribute("searchText", searchText);
         return "category/list";
     }
 
@@ -47,19 +54,67 @@ public class CategoryController {
 //        var category = categories.stream().filter(c -> c.getId() == id).findFirst()
 //                .orElseThrow(() -> new IllegalArgumentException("Invalid category id: " + id));
         Category category = null;
-        for(Category c : categories) {
-            if(c.getId() == id) {
+        for (Category c : categories) {
+            if (c.getId() == id) {
                 category = c;
                 break;
             }
         }
-        if(category == null) {
+        if (category == null) {
             throw new IllegalArgumentException("Invalid category id: " + id);
         }
 
 
         model.addAttribute("category", category);
-        return "category/new";
+        return "category/edit";
     }
 
+    @PostMapping("/{id}/edit")
+    public String editCategory(@PathVariable int id, @ModelAttribute Category category) {
+
+        if (category.getId() != id) {
+            return "redirect:/categories";
+        }
+
+        /*
+            UPDATE Category
+            SET Name=?name, Description=?description
+            WHERE Id=?id
+
+         */
+        for (Category c : categories) {
+            if (c.getId() == id) {
+                c.setName(category.getName());
+                c.setDescription(category.getDescription());
+            }
+        }
+
+
+        // validime {id} a eshte e njejte me {id} e kategorise
+        // a ekziston kjo category me kete ID
+
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/{id}/show")
+    public String showCategory(@PathVariable int id, Model model) {
+        Category category = categories.stream().filter(c -> c.getId() == id).findFirst().orElse(null);
+        model.addAttribute("category", category);
+        return "category/show";// -> templates/category/show.html
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteCategory(@PathVariable int id, Model model) {
+        Category category = categories.stream().filter(c -> c.getId() == id).findFirst().orElse(null);
+        model.addAttribute("category", category);
+        return "category/delete";// -> templates/category/delete.html
+    }
+
+
+    @PostMapping("/{id}/delete")
+    public String deleteCategory(@PathVariable int id) {
+        //fshije nese e gjene nje kategory me id==id
+        categories.removeIf(c -> c.getId() == id);
+        return "redirect:/categories";
+    }
 }
